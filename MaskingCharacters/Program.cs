e2a9e77bd7f7ff8556f3f64aa2a572cf7cbf8a2f
Laura Collins <laura.e.collins1@gmail.com>
2020-02-28 06:43:42 -0600

    done with masking characters

--- MaskingCharacters/Program.cs
+++ MaskingCharacters/Program.cs
@@ -6,7 +6,21 @@ namespace MaskingCharacters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var newString = "";
+
+            Console.WriteLine("Please tell me a secret");
+            var secret = Console.ReadLine();
+
+            for (int i = 0; i < secret.Length - 4; i++)
+            {
+                newString += "X";
+            }
+
+            newString += secret.Substring(secret.Length - 4);
+
+            Console.WriteLine(newString);
+
+            Console.ReadKey();
         }
     }
 }
